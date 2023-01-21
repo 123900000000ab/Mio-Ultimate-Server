b8746070678121efd67a0299a0dc97a79070d566
root-S7 <3020825808@qq.com>
2023-01-21 16:40:22 +0800

    增加静态变量

--- app/src/main/java/com/mio/appStaticVariable/FolderPath.java
+++ app/src/main/java/com/mio/appStaticVariable/FolderPath.java
@@ -1,8 +1,19 @@
 package com.mio.appStaticVariable;
 
+import android.content.Context;
 import android.os.Environment;
 
 public class FolderPath {
-    public static String LAUNCHER_DIR = Environment.getExternalStorageDirectory() + "/MioLauncher";
-    public static String DEFAULT_GAME_DIR = LAUNCHER_DIR + "/.minecraft";
+    public static String defaultMioLauncherDir_Public = Environment.getExternalStorageDirectory() + "/MioLauncher";
+    //默认游戏资源文件存放路径(公有目录)
+    public static String defaultGameDir_Public = defaultMioLauncherDir_Public + "/.minecraft";
+    //游戏资源文件存放路径(私有目录)
+    public static String gameDir_Private;
+    //运行库解压后所在目录
+    public static String runtimeDir_Internal;
+    public FolderPath() {}
+    public FolderPath(Context context){
+        gameDir_Private = context.getExternalFilesDir(".minecraft").getAbsolutePath();
+        runtimeDir_Internal = context.getDir("runtime",Context.MODE_PRIVATE).getAbsolutePath();
+    }
 }
